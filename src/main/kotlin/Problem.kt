828a8e8dffb1b9dbcb005813c18ccd5aea2224c7
Sabinapa <sabina.pauric@gmail.com>
2023-10-23 21:51:15 +0200

    feat: added Trid class and some fix in Problem class

--- src/main/kotlin/Problem.kt
+++ src/main/kotlin/Problem.kt
@@ -3,8 +3,8 @@ abstract class Problem (
     val numberDimension: Int,
     var maxFEs: Int, //maksimalno število ovrednotenj (kolikokrat se lahko pokliče ovrednotenje funkcije)
 ) {
-    val lowerLimit: DoubleArray = DoubleArray(numberDimension) // spodnja meja
-    val upperLimit: DoubleArray = DoubleArray(numberDimension) // zgornja meja
+    var lowerLimit: DoubleArray = DoubleArray(numberDimension) // spodnja meja
+    var upperLimit: DoubleArray = DoubleArray(numberDimension) // zgornja meja
     var currentFes: Int = 0
 
     abstract fun evaluate(x: DoubleArray): Double
---
+++ src/main/kotlin/Trid.kt
@@ -0,0 +1,26 @@
+class Trid(numberDimension: Int, maxFEs: Int) : Problem("Trid", numberDimension, maxFEs) {
+
+    init {
+        val lowerLimitValue = DoubleArray(numberDimension) { -numberDimension.toDouble() * numberDimension.toDouble() }
+        val upperLimitValue = DoubleArray(numberDimension) { numberDimension.toDouble() * numberDimension.toDouble() }
+
+        lowerLimit = lowerLimitValue
+        upperLimit = upperLimitValue
+    }
+
+    override fun evaluate(x: DoubleArray): Double {
+        currentFes++
+        assert(currentFes <= maxFEs)
+
+        val n = x.size
+        var term1 = 0.0
+        var term2 = 0.0
+
+        for (i in 0 until n) {
+            term1 += (x[i] - 1)
+            term2 += (x[i] - 1) * (x[i] - 1)
+        }
+
+        return term1 * term1 - term2
+    }
+}
