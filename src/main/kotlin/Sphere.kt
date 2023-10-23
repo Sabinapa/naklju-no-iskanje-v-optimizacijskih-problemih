369a511355daf114be51c159ffb1be4768033e7a
Sabinapa <sabina.pauric@gmail.com>
2023-10-23 18:27:03 +0200

    feat: added Sphere class for Sphere one-criteria problem

---
+++ src/main/kotlin/Sphere.kt
@@ -0,0 +1,22 @@
+class Sphere(numberDimension: Int, maxFEs: Int): Problem("Sphere", numberDimension, maxFEs) {
+
+    init {
+        val lowerLimitValue = -100.0
+        val upperLimitValue = 100.0
+
+        lowerLimit.fill(lowerLimitValue)
+        upperLimit.fill(upperLimitValue)
+    }
+
+    override fun evaluate(x: DoubleArray): Double {
+        currentFes++
+        assert(currentFes <= maxFEs)
+
+        var fitness = 0.0
+        for (i in 0 until numberDimension) {
+            fitness += x[i] * x[i]
+        }
+
+        return fitness
+    }
+}
